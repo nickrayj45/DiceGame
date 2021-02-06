b2ceeb9b913c1c2f974f1eb0b376d1e135ff5bce
Nicholas Jaquez <nrjaquez@gmail.com>
2021-02-06 15:56:51 -0800

    random generator working

---
+++ index.js
@@ -0,0 +1,7 @@
+// Variable for generating random number between 1 and 6
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+
+console.log(randomNumber1)
+
+// document.getElementsByClassName("img1")[0].setAttribute("", "");

039fe17754dc9f860b69627951b81dceec33c78c
Nicholas Jaquez <nrjaquez@gmail.com>
2021-02-06 19:51:41 -0800

    project finalized

--- index.js
+++ index.js
@@ -9,15 +9,20 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
-// Player 2 Code
+// ---------------------------------------------------------------
 
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
 
-var randomImageSource2 = "assets/" + randomNumber2 + ".png";
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "assets/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomDiceImage2);
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-// If which player wins...
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "1️⃣ Player 1 Wins!"
